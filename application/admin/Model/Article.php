6713a746ae7f51cf20a823d4863eb336c5e65b41
wangning <529023364@qq.com>
2017-05-13 14:39:30 +0800

    关联查询文章列表

---
+++ application/admin/Model/Article.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ning
+ * Date: 17/5/11
+ * Time: 下午4:38
+ */
+
+namespace app\admin\model;
+
+use think\Db;
+use app\admin\model\Common;
+
+class Article extends Common
+{
+    protected $name = 'admin_article';
+    protected  $relationModel = ['admin_menu'];
+    protected $fk = 'id';
+    public function pageList($data = array()){
+        return  $this->field('cms_admin_menu.title as mtitle,cms_admin_article.*')->join("cms_admin_menu","cms_admin_menu.id = cms_admin_article.mid")->where($data)->order('update_time', 'desc')->select();
+    }
+
+}
---
+++ application/admin/controller/Article.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ning
+ * Date: 17/5/9
+ * Time: 上午9:30
+ */
+
+
+namespace app\admin\controller;
+
+use app\common\controller\CommonController;
+use com\Tree;
+use think\Controller;
+use think\Db;
+use app\admin\model\Article as ArticleModel;
+
+class Article extends Controller
+{
+    public function index()
+    {
+        $id = input('post.id','0');
+        $articleModel = new ArticleModel;
+        $re=$articleModel->pageList();
+        echo json_encode(array('code'=>1,'data'=>$re));die;
+    }
+
+    public function add()
+    {
+
+        $data = input();
+        $id = createId();
+        $data['id'] = $id;
+        $menuModel = new ArticleModel;
+        $re=$menuModel->createData($data);
+        if($re){
+            echo json_encode(array('code'=>1,'message'=>'添加成功','data'=>$id));die;
+        }else{
+            echo json_encode(array('code'=>0,'message'=>$menuModel->getError()));die;
+        }
+    }
+
+
+    public function edit()
+    {
+
+        $data = input();
+        $menuModel = new ArticleModel;
+        $re=$menuModel->updateDataById($data,$data['id']);
+        if($re){
+            echo json_encode(array('code'=>1,'message'=>'编辑成功'));die;
+        }else{
+            echo json_encode(array('code'=>0,'message'=>$menuModel->getError()));die;
+        }
+    }
+
+    public function del()
+    {
+        $data = input();
+        $menuModel = new ArticleModel;
+        if(is_array($data['id'])){
+            $idList = $data['id'];
+        }else{
+            $idList = [$data['id']];
+        }
+        $re=$menuModel->delDatas($idList,true);
+        if($re){
+            echo json_encode(array('code'=>1,'message'=>'删除成功'));die;
+        }else{
+            echo json_encode(array('code'=>0,'message'=>$menuModel->getError()));die;
+        }
+    }
+    //查询菜单属性结构图
+    public function tree(){
+        $tree =  new Tree();
+        $re=Db::name('admin_menu')->select();
+        $treeData=$tree->toFormatTree($re);
+        echo json_encode(array('code'=>1,'data'=>$treeData));die;
+
+    }
+
+    //根据菜单id进行显示隐藏
+    public function changestatus(){
+        $id = input('id',0);
+        $status = input('status',1);
+        $menuModel = new ArticleModel;
+//        $idList =$menuModel->getAllChild($id);
+//        array_push($idList,$id);
+//        $re=Db::name('admin_menu')->where('id','in',$idList)->update(array('status'=>$status));
+        $re=$menuModel->enableDatas([$id],$status,true);
+        if($re){
+            echo json_encode(array('code'=>1,'message'=>'状态更新成功'));die;
+        }else{
+            echo json_encode(array('code'=>0,'message'=>'状态更新失败'));die;
+
+        }
+    }
+}
