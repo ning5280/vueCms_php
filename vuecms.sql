20fba9caedf2bd90417bc5820f6533688fb72d22
wangning <529023364@qq.com>
2017-05-14 12:05:59 +0800

    同步下sql数据文件

--- vuecms.sql
+++ vuecms.sql
@@ -1,22 +1,57 @@
 /*
- Navicat MySQL Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         :  本地
- Source Server Version : 50711
- Source Host           : localhost
- Source Database       : vuecms
+Source Server         : 11
+Source Server Version : 50617
+Source Host           : localhost:3306
+Source Database       : vuecms
 
- Target Server Version : 50711
- File Encoding         : utf-8
+Target Server Type    : MYSQL
+Target Server Version : 50617
+File Encoding         : 65001
 
- Date: 05/12/2017 18:07:29 PM
+Date: 2017-05-14 12:04:24
 */
 
-SET NAMES utf8;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
---  Table structure for `cms_admin_group`
+-- Table structure for `cms_admin_article`
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_admin_article`;
+CREATE TABLE `cms_admin_article` (
+  `id` char(32) NOT NULL,
+  `title` varchar(64) NOT NULL DEFAULT '' COMMENT '文章名称',
+  `keyword` varchar(64) DEFAULT NULL COMMENT '关键字',
+  `istop` tinyint(1) DEFAULT '0' COMMENT '是否置顶',
+  `author` varchar(16) DEFAULT '',
+  `img` varchar(64) DEFAULT NULL COMMENT '缩略图',
+  `content` text COMMENT '内容',
+  `create_time` int(11) DEFAULT NULL COMMENT '创建时间',
+  `update_time` int(11) DEFAULT NULL COMMENT '更新时间',
+  `status` tinyint(2) DEFAULT '1' COMMENT '0隐藏1显示2回收',
+  `mid` char(32) NOT NULL DEFAULT '0' COMMENT '所属菜单',
+  `uid` char(32) DEFAULT '0' COMMENT '创建人',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of cms_admin_article
+-- ----------------------------
+INSERT INTO `cms_admin_article` VALUES ('05c06795f6a8c6678861751ab24fb60d', '123123', '123', '0', '213', null, '', '1494732217', '1494732217', '1', 'c7c2cd24dc9881a11a06016c13d31f34', '0');
+INSERT INTO `cms_admin_article` VALUES ('0fefeecebae830c568c548deffcbc3fd', '123213', '123', '0', '3123', null, '', '1494729023', '1494729023', '1', 'c7c2cd24dc9881a11a06016c13d31f34', '0');
+INSERT INTO `cms_admin_article` VALUES ('31b7cc8454505b809f5a1124faf6d11f', '123213', '123', '0', '123213', '123213', '123123123', '123213213', '123213213', '1', '2a205eecf9e4e2ebcbc878f0ed3a4db2', '0');
+INSERT INTO `cms_admin_article` VALUES ('35e987acd15de3df79941fd928d4d210', '测试文章', '123', '0', '123', '20170514\\371f0687578486af3e4c87b73653f3d2.jpg', '&lt;p&gt;123213213213&lt;/p&gt;', '1494728231', '1494728231', '1', '0', '0');
+INSERT INTO `cms_admin_article` VALUES ('44dee2e539f0a95d625bd29fd8c117b1', '123123213', '12312', '0', '3123', null, '', '1494728859', '1494728859', '1', '2a205eecf9e4e2ebcbc878f0ed3a4db2', '0');
+INSERT INTO `cms_admin_article` VALUES ('4d8db461cac5c191011ba2e4555e546e', '123123213', '12312', '1', '3123', null, '', '1494728880', '1494728880', '1', '2a205eecf9e4e2ebcbc878f0ed3a4db2', '0');
+INSERT INTO `cms_admin_article` VALUES ('593baf7a67d251c6dac7ddeddfcbe28a', '123213', '123', '0', '123', null, '', '1494729004', '1494729004', '1', 'c7c2cd24dc9881a11a06016c13d31f34', '0');
+INSERT INTO `cms_admin_article` VALUES ('9e43b8fd676822dac0d856c7e8ba3c6e', '123', '123', '0', '123', null, '', '1494728970', '1494728970', '1', 'c7c2cd24dc9881a11a06016c13d31f34', '0');
+INSERT INTO `cms_admin_article` VALUES ('aed6d5a4967852c63a9979607c7b9436', '123123', '123', '0', '123', null, '', '1494728942', '1494728942', '1', 'c7c2cd24dc9881a11a06016c13d31f34', '0');
+INSERT INTO `cms_admin_article` VALUES ('c6c2df34aabe183ac02b9c0dd6c04308', '42134213213', '123', '0', '213213', null, '', '1494732225', '1494732225', '1', '2a205eecf9e4e2ebcbc878f0ed3a4db2', '0');
+INSERT INTO `cms_admin_article` VALUES ('e5c7d9d33629085688252c5a9466b84e', '123123', '123', '0', '123', null, '', '1494728574', '1494728574', '1', 'c7c2cd24dc9881a11a06016c13d31f34', '0');
+
+-- ----------------------------
+-- Table structure for `cms_admin_group`
 -- ----------------------------
 DROP TABLE IF EXISTS `cms_admin_group`;
 CREATE TABLE `cms_admin_group` (
@@ -30,7 +65,11 @@ CREATE TABLE `cms_admin_group` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `cms_admin_menu`
+-- Records of cms_admin_group
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `cms_admin_menu`
 -- ----------------------------
 DROP TABLE IF EXISTS `cms_admin_menu`;
 CREATE TABLE `cms_admin_menu` (
@@ -49,14 +88,16 @@ CREATE TABLE `cms_admin_menu` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='【配置】后台菜单表';
 
 -- ----------------------------
---  Records of `cms_admin_menu`
+-- Records of cms_admin_menu
 -- ----------------------------
-BEGIN;
-INSERT INTO `cms_admin_menu` VALUES ('10870559f6b4e76530ec7a602cceac9c', '0', '123213', '', '', null, '1', '1', null, null, null), ('55c12f9f2c64707fa335d4df4cb254a7', '0', '11111', '', '', null, '0', '1', null, null, null), ('a1ddf9d4d58b92388c2ff6f5410fc73b', '0', '12', '', '', null, '0', '1', null, null, null), ('a943051309b67ab66b8e6ca1f4fd6564', 'a1ddf9d4d58b92388c2ff6f5410fc73b', '14', '', '', null, '0', '1', null, null, null), ('c7c2cd24dc9881a11a06016c13d31f34', '55c12f9f2c64707fa335d4df4cb254a7', '11111', '', '', null, '0', '1', null, null, null), ('d4ac8f3ab70974ffed1780a4e35da705', '0', '2222', '', '', null, '1', '1', null, null, null);
-COMMIT;
+INSERT INTO `cms_admin_menu` VALUES ('2a205eecf9e4e2ebcbc878f0ed3a4db2', 'c7c2cd24dc9881a11a06016c13d31f34', '324324', '', '', null, '1', '1', null, null, null);
+INSERT INTO `cms_admin_menu` VALUES ('31b7cc8454505b809f5a1124faf6d11f', 'c7c2cd24dc9881a11a06016c13d31f34', '1234', '', '', '0', '11', '0', '0', '', '');
+INSERT INTO `cms_admin_menu` VALUES ('55c12f9f2c64707fa335d4df4cb254a7', '0', '11111', '', '', '0', '0', '1', '0', '', '');
+INSERT INTO `cms_admin_menu` VALUES ('c7c2cd24dc9881a11a06016c13d31f34', '55c12f9f2c64707fa335d4df4cb254a7', '11111', '', '', null, '0', '1', null, null, null);
+INSERT INTO `cms_admin_menu` VALUES ('d8dc8094b6362842a9f992bea7c1e9b2', '73c58eb8991d1faa255d9a8a4a8c3811', '123123', '', '', null, '1', '1', null, null, null);
 
 -- ----------------------------
---  Table structure for `cms_admin_user`
+-- Table structure for `cms_admin_user`
 -- ----------------------------
 DROP TABLE IF EXISTS `cms_admin_user`;
 CREATE TABLE `cms_admin_user` (
@@ -73,10 +114,6 @@ CREATE TABLE `cms_admin_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Records of `cms_admin_user`
+-- Records of cms_admin_user
 -- ----------------------------
-BEGIN;
 INSERT INTO `cms_admin_user` VALUES ('12321fcdssc21', 'admin', 'e10adc3949ba59abbe56e057f20f883e', '12213', '123213', null, null, null, '1');
-COMMIT;
-
-SET FOREIGN_KEY_CHECKS = 1;
